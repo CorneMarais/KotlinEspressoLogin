a10fc4451560377e1f339cd7e0c71cb79cdc186c
Corne <https://github.com/CorneMarais/KotlinEspressoLogin>
2022-08-18 16:06:24 +0200

    Login username

---
+++ app/src/androidTest/java/com/example/kotlinloginuitest/ExampleInstrumentedTest.kt
@@ -0,0 +1,42 @@
+package com.example.kotlinloginuitest
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import com.example.kotlinloginuitest.ui.login.LoginActivity
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class ExampleInstrumentedTest {
+
+    @get:Rule var mActivityTestRule = ActivityTestRule(LoginActivity::class.java)
+
+    @Test
+    fun  Login(){
+        onView(withId(R.id.username))
+            .perform(typeText("Hallo"))
+
+
+
+
+
+
+    }
+
+}
